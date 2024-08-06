401874eb6898f58117b3b41acfbd28a4cff99cee
Oswaldo García <oswaldo.gs.developer@gmail.com>
2024-08-05 18:56:43 -0600

    Init files

---
+++ sandbox-01/sandbox_01App.swift
@@ -0,0 +1,18 @@
+//
+//  sandbox_01App.swift
+//  sandbox-01
+//
+//  Created by Oswaldo García on 05/08/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct sandbox_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
